a4136d336df43a0a2accdd64aea348b578fdc2b0
Tushar Bhat <165662586+tusharbhat7@users.noreply.github.com>
2024-11-15 17:39:46 +0530

    added new info for appspec

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull tushar70/sample-flaskapp2
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 atushar70/sample-flaskapp2
